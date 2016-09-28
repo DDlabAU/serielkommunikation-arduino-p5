6e1a9928573ee59a34de9f0f6919b20f07c9f038
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2016-09-28 18:50:49 +0200

    Sender værdierne med print og println, da det ikke virker at plusse sammen

--- Opgave 3/Arduino/multipleSplit/multipleSplit.ino
+++ Opgave 3/Arduino/multipleSplit/multipleSplit.ino
@@ -21,13 +21,19 @@ void loop() {
     potVal = analogRead(potPin);
     buttonVal = digitalRead(buttonPin);
 
-    // Her plusser vi vores værdier sammen til en stor tekststreng og sætter det ind i en variabel.
-    // Husk at starte med "", da det gør at man med +-operatoren ligger værdier til strengen i stedet for at forsøge at plusse dem sammen som tal.
-    String toSend = "" + potVal + seperator + buttonVal;
+    // Vi sender vores værdier ved at printe den til serielporten. Vi bruger print og ikke bare println, da vi i processing kigger efter linjeskift (\n) som indikator på endt besked.
+    // Alt det vi sender med print kommer i samme besked, som afsluttes med en println, der fortæller processing at de modtagne værdier er én besked, ved at indsætte et linjeskift (\n) til slut.
+    Serial.print(potVal);
+    Serial.print(seperator);
+    Serial.print(buttonVal);
+    Serial.println();
 
-    // Vi sender vores streng ved at printe den til serielporten. Vi bruger print*ln* og ikke bare print, da vi i processing kigger efter linjeskift (\n) som indikator på endt besked.
-    // println tilføjer automatisk dette linjeskift, i modsætning til print.
-    Serial.println(toSend);
+    // Man kan også tage værdier og sætte dem sammen til én string og så sende den. For at gøre er vi dog nødt til at caste vores værdier først:
+    //
+    // String toSend = (String)potPin + seperator + (String)buttonVal;
+    // Serial.println(toSend);
+
+
 
     // Vi venter 100 milisekunder inden vi starter forfra.
     delay(100);
