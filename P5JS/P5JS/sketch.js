d4e58e1356976a0028427a05b87f8f73089b9065
NBAKH <hr.nbakh@gmail.com>
2017-10-31 14:05:56 +0100

    examples

---
+++ P5JS/P5JS/sketch.js
@@ -0,0 +1,43 @@
+// npm install p5.serialserver
+// node ~/node_modules/p5.serialserver/startserver.js
+
+var serial; 
+var options = {baudrate: 9600};
+var portName = '/dev/cu.usbmodem1411';
+var x1 = 0; 
+var y1 = 0; 
+var x2 = 0; 
+var y2 = 0;
+var xIn = 0; 
+var yIn = 0;
+
+function setup() {
+  createCanvas(800, 800); background(0);
+  serial = new p5.SerialPort();
+  serial.on('data', serialEvent);
+//  serial.on('error', serialError);
+  serial.open(portName, options);
+
+  x1 = window.innerWidth/2;
+  y1 = window.innerHeight/2;
+  x2 = window.innerWidth/2;
+  y2 = window.innerHeight/2; }
+
+function draw() {
+  fill(255); stroke(400);
+
+  if (xIn > 500) x2 += 1; 
+  if (xIn < 300) x2 -= 1; 
+
+  if (yIn > 500) y2 += 1; 
+  if (yIn < 300) y2 -= 1; 
+  
+  line(x1, y1, x2, y2); }
+
+function serialEvent() {
+  var inString = serial.readStringUntil('\r\n');
+
+  if (inString.length > 0 ) {
+    var splitString = split(inString, "//");
+    xIn = Number(splitString[0]);
+    yIn = Number(splitString[1]); } }
